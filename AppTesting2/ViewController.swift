f2d066f1416371507042a7af3990e3ea56121b4c
Sovorn <sovorn@github.com>
2018-10-13 15:59:57 +0700

    Initial Commit

---
+++ AppTesting2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTesting2
+//
+//  Created by Sovorn on 10/13/18.
+//  Copyright © 2018 Sovorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
